ac2cf6722e27d6a7619f5cdd134d170c4611e656
Jacob Rose <jacobdanielrose@protonmail.com>
2023-05-02 10:03:53 +0200

    exercize 3.4

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
     const date = new Date()
     const responseText = `<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`
