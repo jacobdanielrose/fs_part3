152540f01f7d3f9131b3f1590f3db3a53f092147
Jacob Rose <jacobdanielrose@protonmail.com>
2023-05-04 19:24:52 +0200

    fixes

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/api/persons', (req, res) => {
     res.status(200).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
